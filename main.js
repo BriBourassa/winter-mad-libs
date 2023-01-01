44151fce264e9f637d15a02d807849f18033c9a3
Brianne Bourassa <shinybri1011@gmail.com>
2023-01-01 10:18:37 -0700

    style website using CSS

---
+++ main.js
@@ -0,0 +1,20 @@
+var noun1 = document.querySelector('.noun1');
+var place1 = document.querySelector('.place1');
+var num1 = document.querySelector('.num1');
+var noun2 = document.querySelector('.noun2');
+var verb1 = document.querySelector('.verb1');
+var clothing1 = document.querySelector('.clothing1');
+var clothing2 = document.querySelector('.clothing2');
+var beverage1 = document.querySelector('.beverage1');
+var food1 = document.querySelector('.food1');
+var story = document.querySelector('.story-box')
+
+var writeBtn = document.querySelector('button');
+
+writeBtn.addEventListener('click', createStory);
+
+function createStory(){
+
+}
+
+//innerHTML to change each value of X to the input value
